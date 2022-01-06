9dfbf380b38520bc3be08b1574dfadfeac8abef5
samuelrantung <samuelmrantung@gmail.com>
2022-01-06 19:04:34 +0800

    finishing personal dashboard screen

--- src/pages/PersonalDashboard/index.js
+++ src/pages/PersonalDashboard/index.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from 'react-native';
+import ReactNativeCalendarStrip from 'react-native-calendar-strip';
 import {colors, Dompet, fonts, PenjualanBg} from '../../assets';
 import {Gap} from '../../components';
 // import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
@@ -14,9 +15,10 @@ import {Gap} from '../../components';
 // const Tab = createBottomTabNavigator();
 
 const PersonalDashboard = () => {
+  const today = new Date();
   return (
     <ImageBackground style={styles.background} source={PenjualanBg}>
-      <Gap height={40} />
+      <Gap height={20} />
       <View style={styles.container}>
         <View style={styles.topComponentContainer}>
           <Dompet />
@@ -31,11 +33,112 @@ const PersonalDashboard = () => {
             </TouchableOpacity>
           </View>
         </View>
+        <View>
+          <ReactNativeCalendarStrip
+            scrollable={true}
+            leftSelector={[]}
+            rightSelector={[]}
+            dateNumberStyle={{color: colors.white}}
+            dateNameStyle={{color: colors.white}}
+            style={styles.calendar}
+            highlightDateNameStyle={{
+              color: colors.primaryGold,
+            }}
+            highlightDateNumberStyle={{
+              color: colors.primaryGold,
+            }}
+            highlightDateContainerStyle={{
+              backgroundColor: colors.white,
+            }}
+            calendarHeaderStyle={{color: colors.white}}
+            calendarHeaderContainerStyle={styles.calendarHeaderContainer}
+            innerStyle={[]}
+            selectedDate={today}
+          />
+        </View>
+        <View style={styles.dataWrapper}>
+          <Gap height={20} />
+          <View style={styles.dataContainer}>
+            <Text style={styles.dataLabel}>Pemasukkan</Text>
+            <View style={styles.dataTotalContainer}>
+              <Text style={styles.dataLabel}>Rp</Text>
+              <Text style={styles.dataLabel}>2.000.000</Text>
+            </View>
+          </View>
+          <View style={styles.dataContainer}>
+            <Text style={styles.dataLabel}>Pengeluaran</Text>
+            <View style={styles.dataTotalContainer}>
+              <Text style={styles.dataLabel}>Rp</Text>
+              <Text style={styles.dataLabel}>3.000.000</Text>
+            </View>
+          </View>
+          <Gap height={10} />
+          <View style={styles.dataContainer}>
+            <View />
+            <Gap
+              height={1}
+              width={'50%'}
+              backgroundColor={colors.primaryGold}
+            />
+          </View>
+          <Gap height={10} />
+          <View style={styles.dataContainer}>
+            <View />
+            <View style={styles.dataTotalContainer}>
+              <Text style={styles.dataTotalLabel}>Rp</Text>
+              <Text style={styles.dataTotalLabel}>1.000.000</Text>
+            </View>
+          </View>
+          <Gap height={30} />
+          <View style={styles.dataButtonContainer}>
+            <TouchableOpacity style={styles.dataButton}>
+              <Text style={styles.dataButtonLabel}>
+                Lihat Laporan Periode Ini
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <Gap height={30} />
+        <View style={styles.gapBar} />
+        <Gap height={24} />
+        <View style={styles.summaryContainer}>
+          <View style={styles.summaryRowContainer}>
+            <Text style={styles.summaryLabel}>Penarikan</Text>
+            <View style={styles.summaryAmountContainer}>
+              <Text style={styles.summaryLabel}>Rp</Text>
+              <Text style={styles.summaryLabel}>250.000</Text>
+            </View>
+          </View>
+          <View style={styles.summaryRowContainer}>
+            <Text style={styles.summaryLabel}>Hutang</Text>
+            <View style={styles.summaryAmountContainer}>
+              <Text style={styles.summaryLabel}>Rp</Text>
+              <Text style={styles.summaryLabel}>200.000</Text>
+            </View>
+          </View>
+          <View style={styles.summaryRowContainer}>
+            <Text style={styles.summaryLabel}>Gaji</Text>
+            <View style={styles.summaryAmountContainer}>
+              <Text style={styles.summaryLabel}>Rp</Text>
+              <Text style={styles.summaryLabel}>1.500.000</Text>
+            </View>
+          </View>
+          <View style={styles.summaryRowContainer}>
+            <Text style={styles.summaryLabel}>Piutang</Text>
+            <View style={styles.summaryAmountContainer}>
+              <Text style={styles.summaryLabel}>Rp</Text>
+              <Text style={styles.summaryLabel}>300.000</Text>
+            </View>
+          </View>
+          <View style={styles.summaryRowContainer}>
+            <Text style={styles.summaryLabel}>Pengeluaran Lainnya</Text>
+            <View style={styles.summaryAmountContainer}>
+              <Text style={styles.summaryLabel}>Rp</Text>
+              <Text style={styles.summaryLabel}>26.000</Text>
+            </View>
+          </View>
+        </View>
       </View>
-      {/* <Tab.Navigator>
-        <Tab.Screen name="Home" component={Dashboard} />
-        <Tab.Screen name="Settings" component={Signin} />
-      </Tab.Navigator> */}
     </ImageBackground>
   );
 };
@@ -50,13 +153,14 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    paddingHorizontal: 26,
+    // paddingHorizontal: 26,
   },
   topComponentContainer: {
     width: '100%',
     // backgroundColor: 'yellow',
     flexDirection: 'row',
     alignItems: 'center',
+    paddingHorizontal: 26,
     // marginHorizontal: 26,
   },
   totalDompetLabel: {
@@ -88,5 +192,83 @@ const styles = StyleSheet.create({
     color: colors.white,
     fontSize: 15,
     fontFamily: fonts.poppins,
+  },
+
+  calendar: {
+    height: 84,
+    borderBottomWidth: 4,
+    borderBottomColor: 'rgba(255, 164, 91, 0.85)',
+  },
+  calendarHeaderContainer: {marginBottom: 8},
+
+  dataWrapper: {
+    paddingHorizontal: 27,
+  },
+  dataLabel: {
+    color: colors.white,
+    fontFamily: fonts.poppins,
+    fontSize: 15,
+  },
+  dataContainer: {
+    // backgroundColor: 'yellow',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  dataTotalContainer: {
+    // backgroundColor: 'yellow',
+    // flex: 1,
+    width: '50%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  dataTotalLabel: {
+    color: colors.white,
+    fontFamily: fonts.poppinsBold,
+    fontSize: 15,
+  },
+  dataButtonContainer: {
+    alignItems: 'center',
+    paddingHorizontal: 40,
+  },
+  dataButton: {
+    height: 32,
+    backgroundColor: colors.primaryGold,
+    borderRadius: 25,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 5,
+  },
+  dataButtonLabel: {
+    color: colors.white,
+    fontSize: 15,
+    fontFamily: fonts.poppins,
+  },
+
+  gapBar: {
+    width: '100%',
+    height: 30,
+    elevation: 5,
+    backgroundColor: colors.primaryGold,
+  },
+
+  summaryContainer: {
+    // backgroundColor: 'yellow',
+    paddingHorizontal: 27,
+  },
+  summaryRowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 5,
+  },
+  summaryAmountContainer: {
+    flexDirection: 'row',
+    width: '45%',
+    justifyContent: 'space-between',
+  },
+  summaryLabel: {
+    color: colors.white,
+    fontFamily: fonts.poppins,
+    fontSize: 15,
   },
 });
